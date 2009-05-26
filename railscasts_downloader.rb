4c3dd1455ce96ab6640870cd644cf0489aecb379
Maximiliano Guzman <maximiliano.guzman@gmail.com>
2009-05-26 13:19:51 -0300

    initial version

---
+++ railscasts_downloader.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'rss'
+require 'progressbar'
+
+p 'Downloading rss index'
+
+rss_string = open('http://feeds.feedburner.com/railscasts').read
+rss = RSS::Parser.parse(rss_string, false)
+videos_urls = rss.items.map { |it| it.enclosure.url }.reverse
+
+videos_filenames = videos_urls.map {|url| url.split('/').last }
+existing_filenames = Dir.glob('*.mov')
+missing_filenames = videos_filenames - existing_filenames
+p "Downloading #{missing_filenames.size} missing videos"
+
+missing_videos_urls = videos_urls.select { |video_url| missing_filenames.any? { |filename| video_url.match filename } }
+
+download_bar = ProgressBar.new('Downloading',missing_videos_urls.size )
+
+
+missing_videos_urls.each do |video_url|
+  filename = video_url.split('/').last
+  p filename
+  p %x(wget #{video_url} -O #{filename}.tmp )
+  p %x(mv #{filename}.tmp #{filename} )
+  download_bar.inc(1)
+end
+p 'Finished synchronization'
